8677f6b8a2d8d5e0b908147e83e1422df97ebd8c
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2018-12-31 02:45:29 +0300

    Added more tiles. Added road collision

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var config = {
   type: Phaser.AUTO,
   width: 16 * 25,
   height: 16 * 25,
+  pixelArt: true,
   physics: {
     default: "arcade",
     arcade: {
@@ -28,12 +29,15 @@ function preload() {
   this.load.image("medieval-ext", "assets/medieval-ext.png");
   this.load.image("medieval-int", "assets/medieval-int.png");
   this.load.image("dungeon", "assets/dungeon.png");
+  this.load.image("dungeon2", "assets/dungeon2.png");
   this.load.tilemapTiledJSON("City", "assets/map-city.json");
 }
 
 function create() {
   this.map = this.add.tilemap("City");
+  console.log("Loaded map", this.map)
 
+  // Tilesets
   var tilesets = {};
   for (var i = 0; i < this.map.tilesets.length; i++) {
     var tl = this.map.tilesets[i];
@@ -42,6 +46,7 @@ function create() {
     tilesets[tl.name] = t;
   }
 
+  // All layers except player
   for (var i = 0; i < this.map.layers.length; i++) {
     var l = this.map.layers[i];
     var p = l.properties;
@@ -51,13 +56,42 @@ function create() {
     }
     p = pa;
     var tilesets = p.tilesets.split(",");
-    console.log("Loading layer", l.name, "properties", p, "tilesets", tilesets);
-    this.map.createStaticLayer(l.name, tilesets);
+    if (l.name != "player") {
+      console.log("Loading layer", l.name, "properties", p, "tilesets", tilesets);
+      this.map.createStaticLayer(l.name, tilesets);
+    }
   }
 
+  var lava = []
+  var platforms = this.physics.add.staticGroup();
+
+  // Process lava
+  for (var i = 0; i < this.map.objects.length; i++) {
+    var o = this.map.objects[i];
+    console.log("Object layer", o.name, "boxes", o.objects.length)
+    for (var j = 0; j < o.objects.length; j++) {
+      var e = o.objects[j];
+      if (e && e.rectangle && e.visible) {
+        var x = e.x
+        var y = e.y
+        var width = e.width
+        var height = e.height
+        console.log("Collision box", e, x, y, width, height)
+        var s = platforms.create(x, y);
+        s.setSize(width, height);
+        s.setOrigin(0, 0)
+        s.setVisible(false)
+        lava[lava.length] = s;
+      }
+    }
+  }
+
+  // Add player
   player = this.physics.add.sprite(10 * 16, 16 * 16, "dude");
   player.setCollideWorldBounds(true);
   player.setBounce(0.2);
+
+  this.physics.add.collider(player, platforms);
 
   this.anims.create({
     key: "left",
